2901f861983d098eb1ca295812a828870089df3b
RaenonX <raenonx0710@gmail.com>
2022-06-07 19:30:46 -0500

    Updated realtime data structure

--- tcoreapi_mq/message/subscription/realtime.py
+++ tcoreapi_mq/message/subscription/realtime.py
@@ -1,5 +1,27 @@
+from decimal import Decimal
+
 from ._base import SubscriptionDataBase
+from .common import CommonData
 
 
 class RealtimeData(SubscriptionDataBase):
-    pass
+    def __init__(self, data: CommonData):
+        super().__init__(data)
+
+        self.quote: dict[str, str] = self.data.body["Quote"]
+
+    @property
+    def symbol_name(self) -> str:
+        return self.quote["Symbol"]
+
+    @property
+    def security(self) -> str:
+        return self.quote["Security"]
+
+    @property
+    def exchange(self) -> str:
+        return f"{self.quote['Exchange']} ({self.quote['ExchangeName']})"
+
+    @property
+    def last_px(self) -> Decimal:
+        return Decimal(self.quote["TradingPrice"])

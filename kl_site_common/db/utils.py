56b948bf0a764497ce9e6a83170175a5f89f2883
RaenonX <raenonx0710@gmail.com>
2022-07-12 03:56:06 -0500

    Added typing for `start_mongo_transaction()`
    Signed-off-by: RaenonX <raenonx0710@gmail.com>

--- kl_site_common/db/utils.py
+++ kl_site_common/db/utils.py
@@ -1,10 +1,13 @@
 from contextlib import contextmanager
+from typing import ContextManager
+
+from pymongo.client_session import ClientSession
 
 from .const import mongo_client
 
 
 @contextmanager
-def start_mongo_transaction():
+def start_mongo_transaction() -> ContextManager[ClientSession]:
     with mongo_client.start_session(causal_consistency=True) as session:
         with session.start_transaction():
             yield session

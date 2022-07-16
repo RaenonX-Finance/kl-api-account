4f22d52fa4b353c48a6979e44d239dcf7f67d691
RaenonX <raenonx0710@gmail.com>
2022-07-16 16:32:38 -0500

    Added custom json encoder to handle `ObjectId`
    Signed-off-by: RaenonX <raenonx0710@gmail.com>

--- kl_site_common/utils/__init__.py
+++ kl_site_common/utils/__init__.py
@@ -1,4 +1,5 @@
 from .console import print_log, print_warning, print_error, print_socket_event
 from .epoch import get_epoch_sec_time
 from .func_exec import execute_async_function
+from .json_encoder import JSONEncoder
 from .system import set_current_process_to_highest_priority
--- kl_site_server/utils/socket/utils.py
+++ kl_site_server/utils/socket/utils.py
@@ -4,10 +4,12 @@ from typing import Any
 
 from pandas import DataFrame
 
+from kl_site_common.utils import JSONEncoder
+
 
 def df_rows_to_list_of_data(df: DataFrame, columns: dict[str, str]) -> list[dict[str, Any]]:
     return df.rename(columns=columns)[columns.values()].to_dict("records")
 
 
 def dump_and_compress(data: Any) -> bytes:
-    return zlib.compress(json.dumps(data).encode("utf-8"), 1)
+    return zlib.compress(json.dumps(data, cls=JSONEncoder).encode("utf-8"), 1)

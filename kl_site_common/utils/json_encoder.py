74e9ec86f2b06f8ca53879328b1d4a90bc2dc8a7
RaenonX <raenonx0710@gmail.com>
2022-07-17 01:05:58 -0500

    Added JSON handling on `pydantic` models
    Signed-off-by: RaenonX <raenonx0710@gmail.com>

--- kl_site_common/utils/json_encoder.py
+++ kl_site_common/utils/json_encoder.py
@@ -1,10 +1,15 @@
 import json
 
 from bson import ObjectId
+from pydantic import BaseModel
 
 
 class JSONEncoder(json.JSONEncoder):
     def default(self, o):
         if isinstance(o, ObjectId):
             return str(o)
+
+        if isinstance(o, BaseModel):
+            return o.dict()
+
         return json.JSONEncoder.default(self, o)

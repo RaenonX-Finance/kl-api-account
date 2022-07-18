e2352c7fc0727a750196e5c5778a2d7f0ee6bc87
RaenonX <raenonx0710@gmail.com>
2022-07-18 00:11:12 -0500

    Added SR level df emptiness check
    Signed-off-by: RaenonX <raenonx0710@gmail.com>

--- kl_site_server/calc/px_data/sr/main.py
+++ kl_site_server/calc/px_data/sr/main.py
@@ -1,10 +1,15 @@
 from pandas import DataFrame
 
+from kl_site_common.utils import print_warning
 from .fx import sr_levels_range_of_pair, sr_levels_range_of_pair_merged
 from .model import SRLevelsData
 
 
 def calc_support_resistance_levels(df_1k: DataFrame, symbol: str) -> SRLevelsData:
+    if not len(df_1k):
+        print_warning(f"Attempt to calculate SR levels on empty dataframe for {symbol}", force=True)
+        return SRLevelsData(groups=[], basic=[])
+
     return SRLevelsData(
         groups=sr_levels_range_of_pair(df_1k, symbol),
         basic=sr_levels_range_of_pair_merged(df_1k, symbol)

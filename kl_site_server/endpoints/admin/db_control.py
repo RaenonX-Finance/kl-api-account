2c1434b4623886ce5bebbcb4da5bd5b7affd6681
RaenonX <raenonx0710@gmail.com>
2022-08-13 05:43:46 -0500

    Updated return of account expiry to updated account data
    Signed-off-by: RaenonX <raenonx0710@gmail.com>

--- kl_site_server/endpoints/admin/db_control.py
+++ kl_site_server/endpoints/admin/db_control.py
@@ -1,33 +1,48 @@
+from typing import Any
+
 from bson import ObjectId
 from fastapi import Body, Depends
+from pymongo import ReturnDocument
 
-from kl_site_server.db import UserDataModel, auth_db_users, user_db_session
+from kl_site_common.db import PyObjectId
+from kl_site_server.db import Permission, UserDataModel, auth_db_users, user_db_session
 from kl_site_server.utils import generate_bad_request_exception, generate_insufficient_permission_exception
-from .model import AccountData, ExpiryUpdateModel
+from .model import AccountData, BlockedUpdateModel, ExpiryUpdateModel
 from ..auth import get_active_user_by_user_data
 
 
-def get_account_list(user: UserDataModel = Depends(get_active_user_by_user_data)) -> list[AccountData]:
-    executor = auth_db_users.find_one({"_id": user.id})
+def permission_check(executor_uid: PyObjectId | None, permission_required: Permission):
+    if not executor_uid:
+        raise generate_bad_request_exception("Invalid executor UID (None)")
 
-    if not executor or not UserDataModel(**executor).has_permission("account:view"):
-        raise generate_insufficient_permission_exception(["account:view"])
+    executor = auth_db_users.find_one({"_id": executor_uid})
+
+    if not executor or not UserDataModel(**executor).has_permission(permission_required):
+        raise generate_insufficient_permission_exception([permission_required])
+
+
+def user_data_dict_to_account_data(user_data_raw: dict[str, Any], *, online: bool) -> AccountData:
+    data = UserDataModel(**user_data_raw)
+
+    return AccountData(
+        id=str(data.id),
+        username=data.username,
+        permissions=data.permissions,
+        expiry=data.expiry,
+        blocked=data.blocked,
+        admin=data.admin,
+        online=online,
+    )
+
+
+def get_account_list(user: UserDataModel = Depends(get_active_user_by_user_data)) -> list[AccountData]:
+    permission_check(user.id, "account:view")
 
     logged_in_account_ids = {session["account_id"] for session in user_db_session.find()}
 
     ret: list[AccountData] = []
     for data in auth_db_users.find({"_id": {"$ne": user.id}}):
-        data = UserDataModel(**data)
-
-        ret.append(AccountData(
-            id=str(data.id),
-            username=data.username,
-            permissions=data.permissions,
-            expiry=data.expiry,
-            blocked=data.blocked,
-            admin=data.admin,
-            online=data.id in logged_in_account_ids,
-        ))
+        ret.append(user_data_dict_to_account_data(data, online=data["_id"] in logged_in_account_ids))
 
     return ret
 
@@ -35,16 +50,40 @@ def get_account_list(user: UserDataModel = Depends(get_active_user_by_user_data)
 def update_account_expiry(
     user: UserDataModel = Depends(get_active_user_by_user_data),
     expiry_update_data: ExpiryUpdateModel = Body(...),
-) -> None:
-    executor = auth_db_users.find_one({"_id": user.id})
+) -> AccountData:
+    permission_check(user.id, "account:expiry")
 
-    if not executor or not UserDataModel(**executor).has_permission("account:expiry"):
-        raise generate_insufficient_permission_exception(["account:expiry"])
-
-    update_result = auth_db_users.update_one(
+    updated_account = auth_db_users.find_one_and_update(
         {"_id": ObjectId(expiry_update_data.id)},
-        {"$set": {"expiry": expiry_update_data.expiry}}
+        {"$set": {"expiry": expiry_update_data.expiry}},
+        return_document=ReturnDocument.AFTER,
     )
 
-    if not update_result.matched_count:
+    if not updated_account:
         raise generate_bad_request_exception(f"No matching account to update ({expiry_update_data.id})")
+
+    return user_data_dict_to_account_data(
+        updated_account,
+        online=user_db_session.find_one({"account_id": ObjectId(updated_account.id)}) is not None,
+    )
+
+
+def update_account_blocked(
+    user: UserDataModel = Depends(get_active_user_by_user_data),
+    blocked_update_data: BlockedUpdateModel = Body(...),
+) -> AccountData:
+    permission_check(user.id, "account:block")
+
+    updated_account = auth_db_users.find_one_and_update(
+        {"_id": ObjectId(blocked_update_data.id)},
+        {"$set": {"blocked": blocked_update_data.blocked}},
+        return_document=ReturnDocument.AFTER,
+    )
+
+    if not updated_account:
+        raise generate_bad_request_exception(f"No matching account to update ({blocked_update_data.id})")
+
+    return user_data_dict_to_account_data(
+        updated_account,
+        online=user_db_session.find_one({"account_id": ObjectId(updated_account["_id"])}) is not None,
+    )
--- kl_site_server/endpoints/admin/main.py
+++ kl_site_server/endpoints/admin/main.py
@@ -1,5 +1,3 @@
-from typing import TypeVar
-
 from fastapi import APIRouter, Depends
 
 from .db_control import get_account_list, update_account_expiry
@@ -7,15 +5,13 @@ from .model import AccountData
 
 admin_router = APIRouter(prefix="/admin")
 
-T = TypeVar("T")
-
 
 @admin_router.get(
     "/accounts",
     description="Get a list of accounts.",
     response_model=list[AccountData],
 )
-async def get_accounts(accounts: list[AccountData] = Depends(get_account_list)) -> T:
+async def get_accounts(accounts: list[AccountData] = Depends(get_account_list)) -> list[AccountData]:
     return accounts
 
 
@@ -23,5 +19,5 @@ async def get_accounts(accounts: list[AccountData] = Depends(get_account_list)) 
     "/update-expiry",
     description="Update the membership expiry of an account.",
 )
-async def update_expiry(_: None = Depends(update_account_expiry)) -> T:
-    pass
+async def update_expiry(updated_account: AccountData = Depends(update_account_expiry)) -> AccountData:
+    return updated_account

e1645bdddfa457b6c92fa829a9b85e74feb0f32f
RaenonX <raenonx0710@gmail.com>
2022-10-12 06:28:29 +0800

    Added period export of MACD fast
    Signed-off-by: RaenonX <raenonx0710@gmail.com>

--- kl_site_server/utils/__init__.py
+++ kl_site_server/utils/__init__.py
@@ -2,5 +2,8 @@ from .exceptions import (
     generate_bad_request_exception, generate_blocked_exception, generate_insufficient_permission_exception,
     generate_unauthorized_exception,
 )
-from .period import MAX_PERIOD, MAX_PERIOD_NO_EMA, CANDLESTICK_DIR_MACD_SLOW, get_start_offset
+from .period import (
+    CANDLESTICK_DIR_MACD_FAST, CANDLESTICK_DIR_MACD_REQUIRED, CANDLESTICK_DIR_MACD_SIGNAL, CANDLESTICK_DIR_MACD_SLOW,
+    MAX_PERIOD, MAX_PERIOD_NO_EMA, get_start_offset,
+)
 from .socket import *  # noqa

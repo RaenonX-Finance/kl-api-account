4a704b53214a7d0ce1ce965e5403e97baa670db4
RaenonX <raenonx0710@gmail.com>
2022-10-13 10:15:37 +0800

    Improved df aggregation checking
    Signed-off-by: RaenonX <raenonx0710@gmail.com>

--- kl_site_server/calc/px_data/aggregate.py
+++ kl_site_server/calc/px_data/aggregate.py
@@ -10,7 +10,6 @@ _AGGREGATE_DICT: dict[str, Callable | str | list[Callable | str] | dict[str, Cal
     PxDataCol.LOW: "min",
     PxDataCol.CLOSE: "last",
     PxDataCol.VOLUME: "sum",
-    PxDataCol.DATE: "first",
     PxDataCol.DATE_MARKET: "first",
     PxDataCol.EPOCH_SEC_TIME: "first",
     PxDataCol.EPOCH_SEC: "first",  # To keep `PxDataCol.EPOCH_SEC` accessible from the indexer
@@ -20,6 +19,7 @@ _AGGREGATE_IGNORE_COLUMNS: set[str] = {
     PxDataCol.INTERVAL,
     PxDataCol.SYMBOL_COMPLETE,
     PxDataCol.TIMESTAMP,
+    PxDataCol.DATE,
     PxDataCol.EPOCH_SEC,  # Group basis, will be included
 }
 
@@ -37,12 +37,13 @@ def aggregate_df(df_1k: DataFrame, period_min: int) -> DataFrame:
 
     # Column processing check
     aggregated_columns = set(_AGGREGATE_DICT.keys()) | _AGGREGATE_IGNORE_COLUMNS
-    df_1k_columns = set(df_1k.columns)  # `.columns` has overridden `__eq__`
-    if aggregated_columns != df_1k_columns:
+    df_columns = set(df.columns)  # `.columns` has overridden `__eq__`
+    if skipped_columns := df_columns - aggregated_columns:
         raise ValueError(
             f"Not all columns from `df_1k` are processed ({period_min}):\n"
-            f"  1K       : {sorted(df_1k_columns)}\n"
+            f"  1K       : {sorted(df_columns)}\n"
             f"  Aggregate: {sorted(aggregated_columns)}"
+            f"  (Skipped): {sorted(skipped_columns)}"
         )
 
     return df.groupby([PxDataCol.EPOCH_SEC]).agg(_AGGREGATE_DICT)

Fixed tie point partial calculation leaving `nan`
Signed-off-by: RaenonX <raenonx0710@gmail.com>

kl_site_server/calc/px_data_fields/tie_point.py
@@ -1,7 +1,10 @@
+from typing import Any
+
 import numpy as np
 import pandas as pd
 from pandas import DataFrame
 
+from kl_site_common.utils import df_get_last_non_nan_rev_index
 from kl_site_server.enums import PxDataCol
 
 
@@ -13,23 +16,20 @@ def _tie_point_all_na(df: DataFrame) -> DataFrame:
     return df
 
 
-def calc_tie_point_full(df_1k: DataFrame, period_min: int) -> DataFrame:
+def calc_tie_point_full(df: DataFrame, period_min: int) -> DataFrame:
     if period_min >= 1440:  # Period > 1 day, tie point is meaningless
-        return _tie_point_all_na(df_1k)
+        return _tie_point_all_na(df)
 
-    mkt_date_group = df_1k.groupby(PxDataCol.DATE_MARKET)
+    mkt_date_group = df.groupby(PxDataCol.DATE_MARKET)
 
-    df_1k[PxDataCol.MARKET_DATE_HIGH] = mkt_date_group[PxDataCol.HIGH].transform(pd.Series.cummax)
-    df_1k[PxDataCol.MARKET_DATE_LOW] = mkt_date_group[PxDataCol.LOW].transform(pd.Series.cummin)
-    df_1k[PxDataCol.TIE_POINT] = np.add(df_1k[PxDataCol.MARKET_DATE_HIGH], df_1k[PxDataCol.MARKET_DATE_LOW]) / 2
+    df[PxDataCol.MARKET_DATE_HIGH] = mkt_date_group[PxDataCol.HIGH].transform(pd.Series.cummax)
+    df[PxDataCol.MARKET_DATE_LOW] = mkt_date_group[PxDataCol.LOW].transform(pd.Series.cummin)
+    df[PxDataCol.TIE_POINT] = np.add(df[PxDataCol.MARKET_DATE_HIGH], df[PxDataCol.MARKET_DATE_LOW]) / 2
 
-    return df_1k
+    return df
 
 
-def _tie_point_of_index(df: DataFrame, cached_calc_df: DataFrame, base_index: int) -> DataFrame:
-    idx_curr = cached_calc_df.index[base_index]
-    idx_prev = cached_calc_df.index[base_index - 1]
-
+def _tie_point_of_index(df: DataFrame, idx_curr: Any, idx_prev: Any) -> DataFrame:
     cur_close = df.at[idx_curr, PxDataCol.CLOSE]
 
     if df.at[idx_curr, PxDataCol.DATE_MARKET] != df.at[idx_prev, PxDataCol.DATE_MARKET]:
@@ -38,10 +38,10 @@ def _tie_point_of_index(df: DataFrame, cached_calc_df: DataFrame, base_index: in
         df.at[idx_curr, PxDataCol.TIE_POINT] = cur_close
     else:
         df.at[idx_curr, PxDataCol.MARKET_DATE_HIGH] = hi = max(
-            df.at[idx_curr, PxDataCol.MARKET_DATE_HIGH], cur_close
+            df.at[idx_prev, PxDataCol.MARKET_DATE_HIGH], cur_close
         )
         df.at[idx_curr, PxDataCol.MARKET_DATE_LOW] = lo = min(
-            df.at[idx_curr, PxDataCol.MARKET_DATE_LOW], cur_close
+            df.at[idx_prev, PxDataCol.MARKET_DATE_LOW], cur_close
         )
         df.at[idx_curr, PxDataCol.TIE_POINT] = (hi + lo) / 2
 
@@ -49,7 +49,7 @@ def _tie_point_of_index(df: DataFrame, cached_calc_df: DataFrame, base_index: in
 
 
 def calc_tie_point_partial(
-    df: DataFrame, cached_calc_df: DataFrame, close_match_rev_index: int, period_min: int
+    df: DataFrame, cached_calc_df: DataFrame, close_match_rev_idx_on_df: int, period_min: int
 ) -> DataFrame:
     if period_min >= 1440:  # Period > 1 day, tie point is meaningless
         return _tie_point_all_na(df)
@@ -58,8 +58,10 @@ def calc_tie_point_partial(
     df[PxDataCol.MARKET_DATE_LOW] = cached_calc_df[PxDataCol.MARKET_DATE_LOW].copy()
     df[PxDataCol.TIE_POINT] = cached_calc_df[PxDataCol.TIE_POINT].copy()
 
-    for base_index in range(close_match_rev_index, 0):
-        df = _tie_point_of_index(df, cached_calc_df, base_index)
+    nan_rev_index = df_get_last_non_nan_rev_index(df, [PxDataCol.MARKET_DATE_HIGH, PxDataCol.MARKET_DATE_LOW])
+
+    for base_index in range(min(close_match_rev_idx_on_df, nan_rev_index), 0):
+        df = _tie_point_of_index(df, df.index[base_index], df.index[base_index - 1])
 
     return df
 
@@ -70,4 +72,4 @@ def calc_tie_point_last(df: DataFrame, period_min: int) -> DataFrame:
 
         return df
 
-    return _tie_point_of_index(df, df, -1)
+    return _tie_point_of_index(df, df.index[-1], df.index[-2])
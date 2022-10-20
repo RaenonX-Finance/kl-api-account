76f472703e2575349a23320cb148456c36f03ac8
RaenonX <raenonx0710@gmail.com>
2022-10-21 03:16:34 +0800

    Refactored history data refetcher
    Signed-off-by: RaenonX <raenonx0710@gmail.com>

--- kl_site_server/client/touchance/main.py
+++ kl_site_server/client/touchance/main.py
@@ -1,9 +1,6 @@
 import asyncio
-import time
-from datetime import datetime, timedelta
-from threading import Thread
+from datetime import datetime
 
-from kl_site_common.const import DATA_PX_REFETCH_BACKWARD_HOUR, DATA_PX_REFETCH_INTERVAL_SEC
 from kl_site_common.utils import execute_async_function, print_log, print_warning
 from kl_site_server.app import (
     on_error, on_px_data_new_bar_created, on_px_data_updated_market, on_system_time_min_change,
@@ -17,6 +14,7 @@ from tcoreapi_mq.client import TouchanceApiClient
 from tcoreapi_mq.message import HistoryData, HistoryInterval, RealtimeData, SystemTimeData
 from tcoreapi_mq.model import SymbolBaseType
 from .calc_data import CalculatedDataManager
+from .refetch import HistoryDataRefetcher
 
 
 class TouchanceDataClient(TouchanceApiClient):
@@ -27,7 +25,16 @@ class TouchanceDataClient(TouchanceApiClient):
         self._px_request_params: dict[str, TouchancePxRequestParams] = {}
         self._calc_data_manager: CalculatedDataManager = CalculatedDataManager(self._px_data_cache)
 
-        Thread(target=self._history_data_refetcher).start()
+        def refetch_history_data(
+            symbol_obj: SymbolBaseType, interval: HistoryInterval, start: datetime, end: datetime
+        ):
+            self.get_history(symbol_obj, interval, start, end, ignore_lock=True)
+
+        HistoryDataRefetcher(
+            self._px_data_cache,
+            self._px_request_params,
+            refetch_history_data,
+        ).start()
 
     def request_px_data(self, params_list: list[TouchancePxRequestParams], *, re_calc_data: bool) -> None:
         for params in params_list:
@@ -94,33 +101,6 @@ class TouchanceDataClient(TouchanceApiClient):
 
     def get_px_data(self, px_data_configs: set[PxDataConfig]) -> list[PxData]:
         return self._px_data_cache.get_px_data(px_data_configs)
-
-    def _history_data_refetcher(self):
-        while True:
-            time.sleep(DATA_PX_REFETCH_INTERVAL_SEC)
-            if not self._px_data_cache.is_all_px_data_ready():
-                print_warning("Skipped re-fetching history px data - not all px data are ready")
-                continue
-
-            # Create list to avoid size change during iteration error
-            for params in list(self._px_request_params.values()):
-                if is_market_closed(params.symbol_obj.security):
-                    print_log(
-                        f"Skipped re-fetching history of "
-                        f"[yellow]{params.symbol_obj.security}[/] - [red]market closed[/]"
-                    )
-                    continue
-
-                start = datetime.utcnow() - timedelta(hours=DATA_PX_REFETCH_BACKWARD_HOUR)
-                end = datetime.utcnow() + timedelta(minutes=2)
-
-                print_log(f"Re-fetching history of [yellow]{params.symbol_obj.security}[/]")
-
-                if params.period_mins:
-                    self.get_history(params.symbol_obj, "1K", start, end, ignore_lock=True)
-
-                if params.period_days:
-                    self.get_history(params.symbol_obj, "DK", start, end, ignore_lock=True)
 
     def on_received_history_data(self, data: HistoryData) -> None:
         print_log(f"Received history data of [yellow]{data.symbol_complete}[/] at [yellow]{data.data_type}[/]")

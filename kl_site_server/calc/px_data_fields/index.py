12840e2f39d01695f62b62f0d8fb4662264465ff
RaenonX <raenonx0710@gmail.com>
2023-02-01 04:03:30 -0600

    Added error throwing on empty DF to set epoch index
    Signed-off-by: RaenonX <raenonx0710@gmail.com>

--- kl_site_server/calc/px_data_fields/index.py
+++ kl_site_server/calc/px_data_fields/index.py
@@ -4,5 +4,8 @@ from kl_site_server.enums import PxDataCol
 
 
 def calc_set_epoch_index(df: DataFrame):
+    if not df.size:
+        raise ValueError("Empty dataframe while setting epoch index")
+
     df[PxDataCol.DATE] = to_datetime(df[PxDataCol.EPOCH_SEC], utc=True, unit="s")
     df.set_index(DatetimeIndex(df[PxDataCol.DATE]), inplace=True)

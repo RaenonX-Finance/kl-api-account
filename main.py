37df4b03fb7e679d7a3de9ef7b3828e46081bdec
RaenonX <raenonx0710@gmail.com>
2023-03-06 05:02:08 -0600

    Added app name logging on start up
    Signed-off-by: RaenonX <raenonx0710@gmail.com>

--- main.py
+++ main.py
@@ -4,8 +4,9 @@ newrelic.agent.initialize("newrelic.ini")
 
 import os  # noqa: E402
 
+from kl_api_common.env import APP_NAME  # noqa: E402
 from kl_api_common.const import print_configs  # noqa: E402
-from kl_api_common.utils import set_current_process_to_highest_priority  # noqa: E402
+from kl_api_common.utils import print_log, set_current_process_to_highest_priority  # noqa: E402
 from kl_api_account.app import start_server_app  # noqa: E402
 from kl_api_account.const import fast_api  # noqa: E402
 
@@ -15,6 +16,8 @@ async def startup_event():
     print_configs()
     start_server_app()
     set_current_process_to_highest_priority()
+
+    print_log(f"App name: [blue]{APP_NAME}[/]", appName=APP_NAME)
 
 
 if __name__ == "__main__":
